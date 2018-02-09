11b5509182f0ea16c3a168d829e428b0122216d7
King Cheng <ckykingkk@gmail.com>
2018-02-09 11:01:11 -0500

    service bean and paymentinfo bean finished

---
+++ src/main/java/com/revature/beans/PaymentInfo.java
@@ -0,0 +1,92 @@
+package com.revature.beans;
+
+public class PaymentInfo {
+
+	private int id;
+	private double cost;
+	private int userId;
+	private int creditCardNumber;
+	
+	public PaymentInfo() {
+		super();
+	}
+
+	public PaymentInfo(int id, double cost, int userId, int creditCardNumber) {
+		super();
+		this.id = id;
+		this.cost = cost;
+		this.userId = userId;
+		this.creditCardNumber = creditCardNumber;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getCost() {
+		return cost;
+	}
+
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getCreditCardNumber() {
+		return creditCardNumber;
+	}
+
+	public void setCreditCardNumber(int creditCardNumber) {
+		this.creditCardNumber = creditCardNumber;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(cost);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + creditCardNumber;
+		result = prime * result + id;
+		result = prime * result + userId;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PaymentInfo other = (PaymentInfo) obj;
+		if (Double.doubleToLongBits(cost) != Double.doubleToLongBits(other.cost))
+			return false;
+		if (creditCardNumber != other.creditCardNumber)
+			return false;
+		if (id != other.id)
+			return false;
+		if (userId != other.userId)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "PaymentInfo [id=" + id + ", cost=" + cost + ", userId=" + userId + ", creditCardNumber="
+				+ creditCardNumber + "]";
+	}
+}
