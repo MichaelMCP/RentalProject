3463c1183821db89f611627243d787cab5a5571f
JasonSteinberg <jasstein403@gmail.com>
2018-02-11 16:24:35 -0500

    service hibernate

--- src/main/java/com/revature/beans/Service.java
+++ src/main/java/com/revature/beans/Service.java
@@ -1,8 +1,24 @@
 package com.revature.beans;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="services")
 public class Service {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="service")
+	@SequenceGenerator(name="service", sequenceName="service_key", allocationSize=1)
 	private int id;
+	@Column(name="service_name")
 	private String serviceName;
+	@Column(name="service_cost")
 	private double serviceCost;
 	
 	public Service() {
