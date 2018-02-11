5cf8ac9b440e43340521c2e4509fda50a07899c0
JasonSteinberg <jasstein403@gmail.com>
2018-02-11 16:49:05 -0500

    service dao1

---
+++ src/main/java/com/revature/data/ServiceHibernate.java
@@ -0,0 +1,20 @@
+package com.revature.data;
+
+import java.util.Set;
+
+import com.revature.beans.Service;
+
+public class ServiceHibernate implements ServiceDao{
+	
+	public Service getService(Service serv) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Service> getServices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
