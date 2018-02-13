fd7d49d637d2251131d0ed02573a09ae45452605
RandallBurnett <rburnettjunior@gmail.com>
2018-02-13 11:26:02 -0500

    homecontroller

--- src/main/java/com/revature/controller/HomeController.java
+++ src/main/java/com/revature/controller/HomeController.java
@@ -3,10 +3,12 @@ package com.revature.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@CrossOrigin(origins="http://50.207.204.190:4200")
 public class HomeController {
 	@RequestMapping(value="/home", method=RequestMethod.GET)
 	public String getHomepage(HttpSession s) {
---
+++ src/main/java/com/revature/controller/LoginController.java
@@ -0,0 +1,45 @@
+package com.revature.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.revature.beans.User;
+import com.revature.service.LoginService;
+
+@Controller
+@CrossOrigin(origins="http://50.207.204.190:4200")
+@RequestMapping(value="/login")
+public class LoginController {
+	@Autowired
+	private LoginService login;
+	
+	public void setLogin(LoginService login) {
+		this.login = login;
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String goLogin(HttpSession session) {
+		if(session.getAttribute("user")!=null)
+			return "home";
+		return "static/login.html";
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public String login(String username, String password, HttpSession session) {
+		System.out.println(username);
+		System.out.println(password);
+		User u = login.login(username, password);
+		if(u == null) {
+			return "redirect:login";
+		} else {
+			session.setAttribute("user", u);
+			return "redirect:home";
+		}
+			
+	}
+}
