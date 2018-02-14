88d7429f11703ff262d096378a3ee613d075dfe4
Michael McPherson <mcpmichaelmcp@gmail.com>
2018-02-14 12:52:36 -0500

    fixed xorigin?

--- src/main/java/com/revature/controller/LoginController.java
+++ src/main/java/com/revature/controller/LoginController.java
@@ -14,7 +14,7 @@ import com.revature.beans.User;
 import com.revature.service.LoginService;
 
 @Controller
-@CrossOrigin(origins= {"http://localhost:4200", "http://18.219.120.2:8080"})
+@CrossOrigin(origins= {"*"})
 @RequestMapping(value="/login")
 public class LoginController {
 	@Autowired
