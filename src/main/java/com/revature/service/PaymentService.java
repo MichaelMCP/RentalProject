71bb9d833d183e4355e378d2f39b406f5d8af4bb
King Cheng <ckykingkk@gmail.com>
2018-02-20 09:50:12 -0500

    fixed conflict

--- src/main/java/com/revature/service/PaymentService.java
+++ src/main/java/com/revature/service/PaymentService.java
@@ -1,9 +1,5 @@
 package com.revature.service;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 91da524b89c4b9203c8aad2af53a485634702e0d
 import com.revature.beans.PaymentInfo;
 
 public interface PaymentService {
