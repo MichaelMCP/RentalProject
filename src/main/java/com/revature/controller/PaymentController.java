4495cdbde0a8b69c945d3294c7c27b361129a6a8
RandallBurnett <rburnettjunior@gmail.com>
2018-02-20 11:45:56 -0500

    payment

--- src/main/java/com/revature/controller/PaymentController.java
+++ src/main/java/com/revature/controller/PaymentController.java
@@ -2,7 +2,6 @@ package com.revature.controller;
 
 import javax.servlet.http.HttpSession;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,15 +13,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.beans.PaymentInfo;
+import com.revature.beans.User;
 import com.revature.service.PaymentService;
-import com.revature.service.RegisterUserService;
 
 @Controller
 @CrossOrigin(origins= {"*"})
-@RequestMapping(value="/Payment")
+@RequestMapping(value="/paymentHandler")
 public class PaymentController {
 	private ObjectMapper om = new ObjectMapper();
-	private Logger log = Logger.getLogger(PaymentController.class);
 	@Autowired
 	private PaymentService payment;
 	
@@ -33,8 +31,11 @@ public class PaymentController {
 	@RequestMapping(method=RequestMethod.POST)
 	@ResponseBody
 	public String addPayment(@RequestBody PaymentInfo paymentInfo, HttpSession session) throws JsonProcessingException {
-		PaymentInfo pyInfo= payment.savePaymentInfo(paymentInfo);
-		log.error(paymentInfo);
+		User u = null;
+		u = (User) session.getAttribute("user");
+		PaymentInfo pyInfo = null;
+		pyInfo.setUser(u);
+		pyInfo= payment.savePaymentInfo(paymentInfo);
 		return om.writeValueAsString(pyInfo);
 			
 	}
